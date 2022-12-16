6deef16d5667058c08dd624a4d89aeb61cb189bb
Duc Tan Hoang <89440534+tanhoang14@users.noreply.github.com>
2022-12-15 17:10:25 -0800

    Update LongestCommonPrefix.java

--- LongestCommonPrefix.java
+++ LongestCommonPrefix.java
@@ -3,13 +3,21 @@ class Solution {
         if(strs.length == 0){
             return "";
         }
-        //prefix = flower
-        String prefix = strs[0];
-        for(int i=1; i < strs.length; i++){
-            while(strs[i].indexOf(prefix) !=0){
-                prefix = prefix.substring(0, prefix.length()-1);
-            }
+     // Set the initial prefix to the first string in the array
+    String prefix = strs[0];
+
+    // Iterate through the remaining strings in the array
+    for(int i=1; i < strs.length; i++){
+
+        // Check if the current string has the prefix as a prefix
+        while(strs[i].indexOf(prefix) !=0){
+    
+            // If it doesn't, remove the last character from the prefix and try again
+            prefix = prefix.substring(0, prefix.length()-1);
         }
+    }
+
+// After the loop, the variable "prefix" will contain the longest common prefix shared by all strings in the array
         return prefix;
     }
 }
